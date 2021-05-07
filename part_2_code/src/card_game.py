b208fa55c7a941dbeb2deab8ad2abdbbd1000fe4
klamb95 <klamb1995@gmail.com>
2021-05-07 12:54:05 +0100

    added screenshots

--- part_2_code/src/card_game.py
+++ part_2_code/src/card_game.py
@@ -6,12 +6,15 @@
 # Carry out dynamic testing on the code below.
 
 # Correct the errors below that you spotted in task 1.
-#check_for_ace
+
+
 def check_for_ace(card):
   if card.value == 1:
     return True
   else:
     return False
+
+
 
 def highest_card(card_1, card_2):
   if card_1.value > card_2.value:
@@ -20,6 +23,7 @@ def highest_card(card_1, card_2):
     return 'its a draw'
   else:
     return card_2
+
 
 
 def cards_total(cards):
